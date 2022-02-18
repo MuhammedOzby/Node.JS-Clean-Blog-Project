24b7dbe32cb42b70f1f95fe62ea3c8b27a759e34
MuhammedOzby <muhammed.ozby@outlook.com>
2022-02-18 18:01:45 +0300

    Before the deployment.

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(
-  //'mongodb://cleandb:plQ3s9SSnE2bd6QC@localhost/cleanblog-test-db',
-  'mongodb+srv://cleandb:plQ3s9SSnE2bd6QC@cluster0.lnjvx.mongodb.net/cleanblog-test-db?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    //'mongodb://cleandb:plQ3s9SSnE2bd6QC@localhost/cleanblog-test-db',
+    'mongodb+srv://cleandb:plQ3s9SSnE2bd6QC@cluster0.lnjvx.mongodb.net/cleanblog-test-db?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Connection succesful.');
+  })
+  .catch((err) => {
+    console.log('Connection error. \n' + err);
+  });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
